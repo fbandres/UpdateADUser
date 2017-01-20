deb15f1c797fe122f76fc57f07c30d1f187bae7e
Francis Paulo Andres <fbandres17@gmail.com>
2017-01-20 22:35:10 +0800

    Add files via upload

---
+++ UpdateADUser.ps1
@@ -0,0 +1,12 @@
+﻿# Set value to businessCategory attribute to all target users inside the CSV file
+
+Import-Module -Name ActiveDirectory
+
+$ADUserList = Import-CSV C:\Users\Francis\Desktop\userlist.csv
+
+$ADUserList | ForEach-Object {Get-ADUser -Filter "SamAccountName -eq `"$($_.SamAccountName)`"" | Set-ADUser -Add @{businessCategory="IT Department"}}
+
+
+# Query all target users after adding value in businessCategory attribute
+
+$ADUserList | ForEach-Object {Get-ADUser -Filter "SamAccountName -eq `"$($_.SamAccountName)`"" -Properties * | Select-Object Name, SamAccountName, @{N="BusinessCategory";E={$_ | Select-Object -ExpandProperty businessCategory}}}
